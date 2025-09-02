603760ec4d0efa4d89c0cc68331cc8e538609494
Bofi1 <angelborque03github@gmail.com>
2025-09-01 20:43:23 -0700

    mobile

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        LibreFranklin: ["'Libre Franklin'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
