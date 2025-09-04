49c923b402ba12f463671a68fea00fb5eb4339da
Bofi1 <angelborque03github@gmail.com>
2025-09-03 21:12:14 -0700

    desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        desktop: "1000px",
+      },
       fontFamily: {
         LibreFranklin: ["'Libre Franklin'", "sans-serif"],
       },
